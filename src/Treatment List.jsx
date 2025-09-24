5f46a7a5e4a9a7e50487525eb4337d90aea16aab
manikanta9040 <manikanta@MacBookAir.Vortex.local>
2025-09-24 15:14:06 +0530

    message

---
+++ src/Treatment List.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+function TreatmentList() {
+  return (
+    <section>
+        
+<section class="spine-treatments">
+  <div class="containss">
+    <h2 class="section-title">Spine Treatments</h2>
+    
+    <div class="treatment-grid">
+      <div class="treatment-item">
+        <img src="https://demo-thelaserspine-gfb2f4hwcqf2dse2.canadacentral-01.azurewebsites.net/assets/images/spine1.jpg" alt="Ultra Minimally Invasive"/>
+        <p><strong>Ultra Minimally Invasive<br/>Spine Surgery</strong></p>
+      </div>
+
+      <div class="treatment-item">
+        <img src="https://demo-thelaserspine-gfb2f4hwcqf2dse2.canadacentral-01.azurewebsites.net/assets/images/spine2.jpg" alt="Minimally Invasive"/>
+        <p><strong>Minimally Invasive<br/>Spine Surgery</strong></p>
+      </div>
+
+      <div class="treatment-item">
+        <img src="https://demo-thelaserspine-gfb2f4hwcqf2dse2.canadacentral-01.azurewebsites.net/assets/images/spine3.jpg" alt="Artificial Disc Replacement"/>
+        <p><strong>Artificial Laser<br/>Replacement</strong></p>
+      </div>
+
+      <div class="treatment-item">
+        <img src="https://demo-thelaserspine-gfb2f4hwcqf2dse2.canadacentral-01.azurewebsites.net/assets/images/spine4.jpg" alt="Endoscopic Spine Surgery"/>
+        <p><strong class="highlight">Endoscopic Spine<br/>Surgery</strong></p>
+      </div>
+
+      <div class="treatment-item">
+        <img src="https://demo-thelaserspine-gfb2f4hwcqf2dse2.canadacentral-01.azurewebsites.net/assets/images/spine5.jpg" alt="Spine Conditions"/>
+        <p><strong>Spine Conditions</strong></p>
+      </div>
+    </div>
+
+    <div class="treatment-btn">
+      <a href="#" class="explore-btn">Explore All Treatments</a>
+    </div>
+  </div>
+
+</section>
+    </section>
+  )
+}
+
+export default TreatmentList
---
+++ src/TreatmentCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function TreatmentCard() {
+  return (
+    <div>
+      
+ <section class="news">
+    <div class="news-cards">
+      
+      <div class="card">
+        <img src="https://demo-thelaserspine-gfb2f4hwcqf2dse2.canadacentral-01.azurewebsites.net/assets/images/news1.jpg" alt="News 1"/>
+        <div class="date">August 6, 2025</div>
+        <h3>DISC Surgeons Awarded – America’s Leading Doctors for 2025</h3>
+        <p>Celebrating Excellence: Drs. Chris Yeung, Justin Field, and Nima Salari Named America’s Leading Doctors 2025 We are thrilled to announce that Drs....</p>
+        <a href="#" class="read-more">Read More</a>
+      </div>
+
+      <div class="card">
+        <img src="https://demo-thelaserspine-gfb2f4hwcqf2dse2.canadacentral-01.azurewebsites.net/assets/images/news2.jpg" alt="News 2"/>
+        <div class="date">July 16, 2025</div>
+        <h3>Congratulations Dr. Field</h3>
+        <p>Congratulations to Dr. Justin Field on recently passing the milestone of 25 patients treated with Barricaid, a pioneering annular closure...</p>
+        <a href="#" class="read-more">Read More</a>
+      </div>
+
+      <div class="card">
+        <img src="https://demo-thelaserspine-gfb2f4hwcqf2dse2.canadacentral-01.azurewebsites.net/assets/images/news3.jpg" alt="News 3"/>
+        <div class="date">July 11, 2025</div>
+        <h3>Selecting the Ideal Spine Surgeon for Endoscopic Spine Surgery</h3>
+        <p></p>
+        <a href="#" class="read-more">Read More</a>
+      </div>
+
+    </div>
+
+    <a href="#" class="view-more">View More</a>
+  </section>
+    </div>
+  )
+}
+
+export default TreatmentCard
